e2fa9721a5436676b159735aca2d353d9bd0cfe7
onesc <ryan@websculptors.net>
2023-10-06 23:49:21 +0900

    Add stupid tags :(

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,30 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>deepfry</title>
+	<meta
+		name="description"
+		content="deep fry, deep fryer, deep fry images, deep fryer online, deepfry"
+	/>
+	<meta
+		name="keywords"
+		content="deep fry, deep fryer, deep fry images, deep fryer online, deep fryer tool, deep fryer app, deep fryer website, deep fryer free, deep fryer free online, deep fryer free tool, deep fryer free app, deep fryer free website, deep fryer free tool online, deep fryer free app online, deep fryer free website online, deep fryer free tool app, deep fryer free tool website, deep fryer free app tool, deep fryer free app website, deep fryer free website tool, deep fryer free website app, deep fryer free tool app online, deep fryer free tool website online, deep fryer free app tool online, deep fryer free app website online, deep fryer free website tool online, deep fryer free website app online, deep fryer free tool app website, deep fryer free tool website app, deep fryer free app tool website, deep fryer free app website tool, deep fryer free website tool app, deep fryer free website app tool, deep fryer free tool app website online, deep fryer free tool website app online, deep fryer free app tool website online, deep fryer free app website tool online, deep fryer free website tool app online, deep fryer free website app tool online"
+	/>
+	<meta name="author" content="deepfry" />
+	<meta name="robots" content="index, follow" />
+	<meta name="googlebot" content="index, follow" />
+	<meta name="google" content="notranslate" />
+	<meta name="format-detection" content="telephone=no" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta property="og:title" content="deepfryer" />
+	<meta
+		property="og:description"
+		content="deep fry, deep fryer, deep fry images, deep fryer online, deepfry"
+	/>
+	<meta property="og:site_name" content="deepfry" />
+	<meta property="og:type" content="website" />
+	<meta property="og:locale" content="en_US" />
+</svelte:head>
+
 <slot />
