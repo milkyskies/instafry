f4e75bebc93924993c448f8c3a65511b74a25cac
onesc <ryan@websculptors.net>
2023-10-07 00:24:50 +0900

    Add favicon, remove my will to live

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,10 @@
 
 <svelte:head>
 	<title>instafry</title>
-	<meta
-		name="description"
-		content="insta fry, insta fryer, insta fry images, insta fryer online, instafry"
-	/>
+	<meta name="description" content="get you're images nice and crispy" />
 	<meta
 		name="keywords"
-		content="insta fry, insta fryer, insta fry images, insta fryer online, insta fryer tool, insta fryer app, insta fryer website, insta fryer free, insta fryer free online, insta fryer free tool, insta fryer free app, insta fryer free website, insta fryer free tool online, insta fryer free app online, insta fryer free website online, insta fryer free tool app, insta fryer free tool website, insta fryer free app tool, insta fryer free app website, insta fryer free website tool, insta fryer free website app, insta fryer free tool app online, insta fryer free tool website online, insta fryer free app tool online, insta fryer free app website online, insta fryer free website tool online, insta fryer free website app online, insta fryer free tool app website, insta fryer free tool website app, insta fryer free app tool website, insta fryer free app website tool, insta fryer free website tool app, insta fryer free website app tool, insta fryer free tool app website online, insta fryer free tool website app online, insta fryer free app tool website online, insta fryer free app website tool online, insta fryer free website tool app online, insta fryer free website app tool online"
+		content="deep fry images, deep fryer, deep fryer online, deep fryer images, deep fryer online, meme, deep fry meme, deepfry"
 	/>
 	<meta name="author" content="instafry" />
 	<meta name="robots" content="index, follow" />
@@ -18,11 +15,8 @@
 	<meta name="google" content="notranslate" />
 	<meta name="format-detection" content="telephone=no" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta property="og:title" content="instafryer" />
-	<meta
-		property="og:description"
-		content="insta fry, insta fryer, insta fry images, insta fryer online, instafry"
-	/>
+	<meta property="og:title" content="instafry" />
+	<meta property="og:description" content="get you're images nice and crispy" />
 	<meta property="og:site_name" content="instafry" />
 	<meta property="og:type" content="website" />
 	<meta property="og:locale" content="en_US" />
